d505dbe29793e30cace826ad61da78b8d1d41221
Wille4321 <wille.nordstrom08@gmail.com>
2024-12-17 13:05:46 +0200

    Add files via upload

---
+++ CSharpExam/Task3 ArraysLoopsAndSort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_ArraysLoopsAndSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Two arrays with 1 containing names and the other containing numbers
+            string[] name = {"bob", "Steve", "Nina"};
+
+            int[] num = {5, 57, 87, 1, 150, 32, 14};
+
+            //A loop that prints out the names with a goodbye before them
+            for(int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine("Goodbye " + name[i]);
+            }
+
+            //A sort that sorts the numbers in ascending order and removes the last element
+            Array.Sort(num);
+
+            num = num.Take(num.Length - 1).ToArray();
+            
+            Console.WriteLine("\nSorted numbers without the last element: ");
+            Console.WriteLine(string.Join(",", num));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpExam/Task6 Classes/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //New list
+            List<Game>games = new List<Game>();
+
+            //Add games to the list
+            Game game1 = new Game("Bloons 6", "Ninja Kiwi", "2018", "Tower Defense");
+            games.Add(game1);
+
+            Game game2 = new Game("Minecraft", "Notch", "2009", "Survival");
+            games.Add(game2);
+
+            //Prints out the list
+            Console.WriteLine("Game list: ");
+            foreach (Game game in games)
+            {
+                game.DisplayInfo();
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    
+    class Game
+    {
+        //class properties
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public string Year { get; set; }
+        public string Genre { get; set; }
+
+
+        public Game(string name, string publisher, string year, string genre)
+        {
+            Name = name;
+            Publisher = publisher;
+            Year = year;
+            Genre = genre;
+        }
+
+        //class that makes the output show what it is
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Publisher: {Publisher}, Year: {Year}, Genre: {Genre}");
+        }
+    }
+}
+
+
